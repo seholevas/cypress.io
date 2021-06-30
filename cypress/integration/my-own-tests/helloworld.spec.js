aa0430bbb0e3e12599c3d0942fae5aa33d30639b
steven <seholevas@gmail.com>
2021-06-29 21:31:00 -0400

    created my own file for tests

---
+++ cypress/integration/my-own-tests/helloworld.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('basic website features',()=>{
+
+    it('Webpage Loads',()=>{
+       cy.visit('https://linkedin.com') 
+    })
+})
+
+
+describe('basic login/create user features work',()=>{
+
+    it('Path to Login Page Works',()=>{
+        cy.contains("Sign in").click()
+        cy.url().should('include','/login')
+        cy.contains("button","Sign in").should("exist")
+        cy.contains("button","Sign in").should("exist")
+    
+        // cy.contains("Forgot Password?").should("exist")
+     })
+
+     it('Path to Join now Page works',()=>{
+        cy.contains("Join now").click()
+        cy.url().should('include','/signup')
+        cy.contains("button","Agree & Join").should("exist")
+        cy.contains("button","Join with Google").should("exist")
+     })
+})
